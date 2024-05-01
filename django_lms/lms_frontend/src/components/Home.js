c994d9f14ec961aa69ab5c3badb3d9c6c29123f4
Deepika <goeld9911@gmail.com>
2024-05-01 17:26:59 +0530

    Started to implement react js into the project on lms_frontend

---
+++ django_lms/lms_frontend/src/components/Home.js
@@ -0,0 +1,12 @@
+
+
+function Home() {
+  return (
+    <div className="App">
+      <h3>Home Page</h3>
+      
+    </div>
+  );
+}
+
+export default Home;
---
+++ django_lms/lms_frontend/src/components/Main.js
@@ -0,0 +1,13 @@
+
+import Home from './Home'
+
+function Main() {
+  return (
+    <div className="App">
+      <Home />
+      
+    </div>
+  );
+}
+
+export default Main;
